1b89c2af80a8ea556157d6e1d5cc9a3e427c4033
Livanovkv <livanovkv@yandex.ru>
2022-11-17 13:16:13 +0300

    Editor

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-//* express
 const express = require('express');
+
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
+const usersRouter = require('./routes/users');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
