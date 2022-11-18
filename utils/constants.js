bd9d49d078292b444db50a95241038808de4c970
Livanovkv <livanovkv@yandex.ru>
2022-11-18 11:58:55 +0300

    fix fix tests

--- utils/constants.js
+++ utils/constants.js
@@ -8,6 +8,6 @@ module.exports.codNotFound = 404;
 
 module.exports.codInternalServerError = 500;
 
-module.exports.createdMessageError = (err) => ({ message: `${err.name}: ${err.message}` });
-
 module.exports.textErrorNoUser = 'Такого пользователя нет';
+
+module.exports.textErrorNoCard = 'Такой карточки нет';
