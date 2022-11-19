76949012526d5f2979740f15ed1a4f611220fbe6
livanovkv <livanovkv@yandex.ru>
2022-11-19 12:42:13 +0300

    Fix singlequotte

--- utils/constants.js
+++ utils/constants.js
@@ -8,8 +8,8 @@ module.exports.CODE_NOT_FOUND = 404;
 
 module.exports.CODE_INTERNAL_SERVER_ERRORE = 500;
 
-module.exports.TEXT_ERRORE_NO_USER = "Такого пользователя нет";
+module.exports.TEXT_ERRORE_NO_USER = 'Такого пользователя нет';
 
-module.exports.TEXT_ERRORE_NO_CARD = "Такой карточки нет";
+module.exports.TEXT_ERRORE_NO_CARD = 'Такой карточки нет';
 
-module.exports.TEXT_ERRORE_DATA = "Переданы некорректные данные";
+module.exports.TEXT_ERRORE_DATA = 'Переданы некорректные данные';
