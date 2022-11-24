155842d18815fdcf9666dbd28f37c475442d887a
livanovkv <94444290+livanovkv@users.noreply.github.com>
2022-11-24 21:45:51 +0300

    fix fix

--- models/user.js
+++ models/user.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema({
         return /^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/.test(value);
       },
     },
-    default: 'https://illustrators.ru/uploads/illustration/image/1207680/20181013_120232.jpg',
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
